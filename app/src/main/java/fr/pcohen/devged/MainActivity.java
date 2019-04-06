7589b68e65c96d0f5d504845bd1fa6513374c881
philippe Cohen <philippe@pcohen.fr>
2019-04-06 23:13:17 +0200

    Basic changes

--- app/src/main/java/fr/pcohen/devged/MainActivity.java
+++ app/src/main/java/fr/pcohen/devged/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Adding a new ressource", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add new ressources", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
