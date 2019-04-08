248ee67b48ab17ba62d8fb84d93d83e4623d8a08
philippe Cohen <philippe@pcohen.fr>
2019-04-08 23:22:23 +0200

    FireBase Login by email with return

--- app/src/main/java/fr/pcohen/devged/MainActivity.java
+++ app/src/main/java/fr/pcohen/devged/MainActivity.java
@@ -1,10 +1,13 @@
 package fr.pcohen.devged;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,8 +18,12 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 
 import java.util.Arrays;
-//import com.firebase.ui.auth.ErrorCodes;
+import java.util.Objects;
+
+import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+
+import butterknife.BindView;
 //import com.openclassrooms.firebaseoc.api.UserHelper;
 //import com.openclassrooms.firebaseoc.auth.ProfileActivity;
 //import com.openclassrooms.firebaseoc.base.BaseActivity;
@@ -26,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button btnConnexion;
     private static final int RC_SIGN_IN = 123;
+
+    @BindView(R.id.main_activity_coordinator_layout) CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +53,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setAction("Action", null).show();
             }
         });
-
-
 
     }
 
@@ -91,13 +98,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        //.setTheme(R.style.LoginTheme)
+                        .setTheme(R.style.LoginTheme)
                         .setAvailableProviders(
                                 Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
                         .setIsSmartLockEnabled(false, true)
                         //.setLogo(R.drawable.ic_logo_auth)
                         .build(),
                 RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                //showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+                Log.d("SUCCESS ************ ", getString(R.string.connection_succeed));
+                Toast.makeText(this, getString(R.string.connection_succeed), Toast.LENGTH_LONG).show();
+            } else { // ERRORS
+                if (response == null) {
+                    //showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                    Log.d("ERR **************", getString(R.string.error_authentication_canceled));
+                    Toast.makeText(this, getString(R.string.error_authentication_canceled), Toast.LENGTH_LONG).show();
+                } else if (Objects.requireNonNull(response.getError()).getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    //showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                    Log.d("ERR **************", getString(R.string.error_no_internet));
+                    Toast.makeText(this, getString(R.string.error_no_internet), Toast.LENGTH_LONG).show();
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    //showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                    Log.d("ERR **************", getString(R.string.error_unknown_error));
+                    Toast.makeText(this, getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
 }
